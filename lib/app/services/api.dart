0e345a1835e20b19327b2998756392ddc3e346b6
Tanmay Bhosale <tanmaybhosale24@gmail.com>
2020-10-18 20:51:47 +0530

    Backend done api and apiClass done

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -2,6 +2,8 @@ import 'package:coronatracker_usemy/app/services/api_keys.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
+enum Endpoint { cases, casesSuspected, casesConfirmed, deaths, recovered }
+
 class API {
   API({@required this.apiKey});
   final String apiKey;
@@ -11,4 +13,15 @@ class API {
   static final host = "ncov2019-admin.firebaseapp.com";
 
   Uri tokenUri() => Uri(scheme: "https", host: host, path: "token");
+
+  Uri endpointUri(Endpoint endpoint) =>
+      Uri(scheme: "https", host: host, path: _paths[endpoint]);
+
+  static Map<Endpoint, String> _paths = {
+    Endpoint.cases: "cases",
+    Endpoint.casesSuspected: "casesSuspected",
+    Endpoint.casesConfirmed: "casesConfirmed",
+    Endpoint.deaths: "deaths",
+    Endpoint.recovered: "recovered"
+  };
 }
--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:coronatracker_usemy/app/services/api.dart';
 
@@ -20,4 +21,33 @@ class APIService {
         "Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}");
     throw response;
   }
+
+  Future<int> getEndpointData(
+      {@required String accessToken, @required Endpoint endpoint}) async {
+    final uri = api.endpointUri(endpoint);
+    final response = await http
+        .get(uri.toString(), headers: {"Authorization": "Bearer $accessToken"});
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final Map<String, dynamic> endpointData = data[0];
+        final String responseJsonKey = _responseJsonKeys[endpoint];
+        final int result = endpointData[responseJsonKey];
+        if (result != null) {
+          return result;
+        }
+      }
+    }
+    print(
+        "Request $uri failed\nResponse: ${response.statusCode} ${response.reasonPhrase}");
+    throw response;
+  }
+
+  static Map<Endpoint, String> _responseJsonKeys = {
+    Endpoint.cases: "cases",
+    Endpoint.casesSuspected: "data",
+    Endpoint.casesConfirmed: "data",
+    Endpoint.deaths: "data",
+    Endpoint.recovered: "data",
+  };
 }
