5b2ba0c2fdd167ca6a068d08eeacda771a642954
Tanmay Bhosale <tanmaybhosale24@gmail.com>
2020-10-19 16:25:02 +0530

    backend done UI in progress

---
+++ lib/app/repositories/data_repository.dart
@@ -0,0 +1,29 @@
+import 'package:coronatracker_usemy/app/services/api.dart';
+import 'package:coronatracker_usemy/app/services/api_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+
+class DataRepository {
+  DataRepository({@required this.apiService});
+  final APIService apiService;
+
+  String _accessToken;
+
+  Future<int> getEndpointData(Endpoint endpoint) async {
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      // if unauthorized, get access token
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+            accessToken: _accessToken, endpoint: endpoint);
+      }
+      rethrow;
+    }
+  }
+}
---
+++ lib/app/ui/dashboard.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+
+class Dashoard extends StatefulWidget {
+  @override
+  _DashoardState createState() => _DashoardState();
+}
+
+class _DashoardState extends State<Dashoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
